52661bbe1794a73a34bacfba98fc2e433ddd912e
Krishnatheja Vanka <theja.vanka@gmail.com>
2019-12-23 23:45:39 +0530

    Create Server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+/*
+*
+* Primary file for the API
+*
+*/
+
+// Dependancies
+let http = require('http');
+
+// The server should respond to all requests with a string
+async function createServerFunc(req,res){
+    return await res.end('Hello World\n');
+};
+let server = http.createServer(createServerFunc);
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, ()=>console.log("The server is listening on port 3000 now"));
