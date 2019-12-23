704eabebf71ab8def4cde68dc2de85d5a642e199
Krishnatheja Vanka <theja.vanka@gmail.com>
2019-12-24 00:38:05 +0530

    Params and Headers

--- index.js
+++ index.js
@@ -9,32 +9,45 @@ const http = require('http');
 const url = require('url');
 
 // The server should respond to all requests with a string
-function trimmedPathFunc(req){
-    // Get the URL and parse it
-    let parsedUrl = url.parse(req.url,true);
-
+function trimmedPathFunc(parsedUrl){
+    
     // Get the path
     let path = parsedUrl.pathname;
     let trimmedPath = path.replace(/^\/+|\/+$/g,'');
     return trimmedPath;
+
 };
 
 async function createServerFunc(req,res){
     
-    // Calling trimmedPathFunc
-    let trimmedPath = await trimmedPathFunc(req,res);
+    try {
+        
+        // Get the URL and parse it
+        let parsedUrl = url.parse(req.url,true);
 
-    // Get the HTTP Method
-    let method = req.method.toUpperCase();
+        // Calling trimmedPathFunc
+        let trimmedPath = await trimmedPathFunc(parsedUrl);
+        // Get the query string as an object
+        let queryStringObject = parsedUrl.query;
 
-    //Send the response
-    res.end('Hello World\n');
+        // Get the HTTP Method
+        let method = req.method.toUpperCase();
 
-    // Log the request path
-    console.log('Request received on path: '+trimmedPath+' with this method : '+method);
+        // Get the headers as an object
+        let headers = req.headers;
+
+        //Send the response
+        res.end('Hello World\n');
+
+        // Log the request path
+        console.log('Request received with these headers',headers);
+    } catch (error) {
+        console.error(error);
+    }
+
 };
-let server = http.createServer(createServerFunc);
 
+let server = http.createServer(createServerFunc);
 
 
 // Start the server, and have it listen on port 3000
